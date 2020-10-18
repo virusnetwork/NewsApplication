d1e026b1aa82473cb3aea46718a47481c65272d6
Miles Singleton <milessingleton@hotmail.co.uk>
2020-10-18 15:41:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/newsapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.newsapplication
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
