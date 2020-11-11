80fae8b8ce8c5e8c842ecf8c72120d3a31b35ace
Miles Singleton <milessingleton@hotmail.co.uk>
2020-11-11 10:02:56 +0000

    added settings window and firebase

---
+++ app/src/main/java/com/example/newsapplication/SettingActivity.kt
@@ -0,0 +1,23 @@
+package com.example.newsapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+
+val signedIn : Boolean = false
+
+class SettingActivity : AppCompatActivity ()
+{
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting_windows)
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/newsapplication/settingsAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.newsapplication
+
+class settingsAdapter :  {
+}
